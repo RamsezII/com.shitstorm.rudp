ba4a05ae07c32a965cc013030932e23e0f26d2c4
ram16 <thomass.montaine@gmail.com>
2024-06-01 18:13:46 +0200

    add host good

--- NETWORK/EveClient/_AddHost.cs
+++ NETWORK/EveClient/_AddHost.cs
@@ -12,17 +12,6 @@ namespace _RUDP_
             Hosting,
         }
 
-        public enum HostCodes : byte
-        {
-            _none_,
-            Added,
-            Exists,
-            Maintained,
-            NotFound,
-            Removed,
-            _last_,
-        }
-
         readonly ThreadSafe<HostStates> hostState = new();
         public static readonly int gameHash = Application.productName.GetHashCode();
         public string hostName;
@@ -43,42 +32,10 @@ namespace _RUDP_
 
         void OnAddHostAck()
         {
-            Debug.Log($"----- Received AddHostAck -----");
-            lock (hostState)
-            {
-                HostCodes code = (HostCodes)socketReader.ReadByte();
-                switch (code)
-                {
-                    case HostCodes.Added:
-                        hostState._value = HostStates.Hosting;
-                        Debug.Log($"Host added: {hostName}");
-                        break;
-
-                    case HostCodes.Exists:
-                        hostState._value = HostStates.Hosting;
-                        Debug.Log($"Host exists: {hostName}");
-                        break;
-
-                    case HostCodes.Maintained:
-                        hostState._value = HostStates.Hosting;
-                        Debug.Log($"Host maintained: {hostName}");
-                        break;
-
-                    case HostCodes.NotFound:
-                        hostState._value = HostStates.Adding;
-                        Debug.Log($"Host not found: {hostName}");
-                        break;
-
-                    case HostCodes.Removed:
-                        hostState._value = HostStates.Adding;
-                        Debug.Log($"Host removed: {hostName}");
-                        break;
-
-                    default:
-                        Debug.LogWarning($"Received unimplemented host code: \"{code}\"");
-                        break;
-                }
-            }
+            bool success = socketReader.ReadBoolean();
+            hostState.Value = success ? HostStates.Hosting : HostStates.Adding;
+            if (success)
+                eveStream.Position = HEADER_LENGTH + 1;
         }
     }
 }
