bf78308a72b721b58df9a1953fa4fbfeeef159ff
ram16 <thomass.montaine@gmail.com>
2024-11-18 17:38:54 +0100

    Update RudpSocket.cs

--- Socket/RudpSocket.cs
+++ Socket/RudpSocket.cs
@@ -35,6 +35,8 @@ namespace _RUDP_
 
         //----------------------------------------------------------------------------------------------------------
 
+        ~RudpSocket() => Debug.Log($"~{this}");
+
         public RudpSocket(in ushort port = 0) : base(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp)
         {
             recStream_u = new(recBuffer_u);
