561e66c7b0b230587c1a848a4c625114a3759719
ram16 <thomass.montaine@gmail.com>
2025-05-11 03:00:24 +0200

    ct quoi cette boucle de merde

--- Runtime/EveComm/_Joining.cs
+++ Runtime/EveComm/_Joining.cs
@@ -11,82 +11,79 @@ namespace _RUDP_
             bool failure = false;
             RudpConnection hostConn = null;
 
-            while (true)
-            {
-                IEnumerator<float> eSend = ESendUntilAck(
-                    writer =>
-                    {
-                        eveWriter.Write((byte)EveCodes.JoinHost);
-                        eveWriter.WriteText(hostName);
-                        eveWriter.Write(publicHash);
-                        eveWriter.WriteIPEnd(conn.socket.selfConn.localEnd);
-                    },
-                    reader =>
-                    {
-                        ReadPublicEnd();
-                        AckCodes ack = (AckCodes)socketReader.ReadByte();
-
-                        switch (ack)
-                        {
-                            case AckCodes.Confirm:
-                                {
-                                    hostConn = conn.socket.ReadConnection(socketReader, out _);
-                                    hostConn.keepAlive = true;
-                                    Debug.Log($"Start Holepunch-> {hostConn.endPoint}");
-                                }
-                                break;
-
-                            case AckCodes.Reject:
-                                Debug.LogWarning("Joining host rejected");
-                                break;
-
-                            case AckCodes.HostNotFound:
-                                Debug.LogWarning("Host not found");
-                                break;
-
-                            case AckCodes.HostPassMismatch:
-                                Debug.LogWarning("Host password mismatch");
-                                break;
-
-                            default:
-                                Debug.LogWarning($"Unexpected ack: \"{ack}\"");
-                                break;
-                        }
-
-                        if (ack != AckCodes.Confirm)
-                        {
-                            onFailure?.Invoke();
-                            failure = true;
-                        }
-                    },
-                    () =>
-                    {
-                        Debug.LogWarning("Failed to start joining");
-                        onFailure?.Invoke();
-                        failure = true;
-                    });
-
-                while (eSend.MoveNext())
-                    yield return eSend.Current;
-
-                lock (mainLock)
-                    if (failure)
-                        yield break;
-
-                var wait = new WaitForSecondsRealtime(1);
-                while (true)
+            IEnumerator<float> eSend = ESendUntilAck(
+                writer =>
                 {
-                    if (hostConn.IsAlive(1000))
+                    eveWriter.Write((byte)EveCodes.JoinHost);
+                    eveWriter.WriteText(hostName);
+                    eveWriter.Write(publicHash);
+                    eveWriter.WriteIPEnd(conn.socket.selfConn.localEnd);
+                },
+                reader =>
+                {
+                    ReadPublicEnd();
+                    AckCodes ack = (AckCodes)socketReader.ReadByte();
+
+                    switch (ack)
                     {
-                        onSuccess?.Invoke(hostConn);
-                        yield break;
+                        case AckCodes.Confirm:
+                            {
+                                hostConn = conn.socket.ReadConnection(socketReader, out _);
+                                hostConn.keepAlive = true;
+                                Debug.Log($"Start Holepunch-> {hostConn.endPoint}");
+                            }
+                            break;
+
+                        case AckCodes.Reject:
+                            Debug.LogWarning("Joining host rejected");
+                            break;
+
+                        case AckCodes.HostNotFound:
+                            Debug.LogWarning("Host not found");
+                            break;
+
+                        case AckCodes.HostPassMismatch:
+                            Debug.LogWarning("Host password mismatch");
+                            break;
+
+                        default:
+                            Debug.LogWarning($"Unexpected ack: \"{ack}\"");
+                            break;
                     }
 
-                    if (wait.MoveNext())
-                        yield return 0;
-                    else
-                        break;
+                    if (ack != AckCodes.Confirm)
+                    {
+                        onFailure?.Invoke();
+                        failure = true;
+                    }
+                },
+                () =>
+                {
+                    Debug.LogWarning("Failed to start joining");
+                    onFailure?.Invoke();
+                    failure = true;
+                });
+
+            while (eSend.MoveNext())
+                yield return eSend.Current;
+
+            lock (mainLock)
+                if (failure)
+                    yield break;
+
+            var wait = new WaitForSecondsRealtime(1);
+            while (true)
+            {
+                if (hostConn.IsAlive(1000))
+                {
+                    onSuccess?.Invoke(hostConn);
+                    yield break;
                 }
+
+                if (wait.MoveNext())
+                    yield return 0;
+                else
+                    break;
             }
         }
     }
